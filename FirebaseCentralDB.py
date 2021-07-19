242dd0f709280ddbd1018d4965aacd407868426a
Sachira Kuruppu <rsachira@live.com>
2021-07-20 07:36:58 +1200

    Script to insert datasets to firebase

--- FirebaseCentralDB.py
+++ FirebaseCentralDB.py
@@ -23,22 +23,58 @@ firebaseConfig = {
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth     = firebase.auth()
 
-email    = input('Enter email: ')
-passw    = input('Enter password: ')
+email    = 'nah_ncbi@sparclink.com'#input('Enter email: ')
+passw    = '123456'#input('Enter password: ')
 user     = auth.sign_in_with_email_and_password(email, passw)
 
+db = firebase.database()
 award_db   = firebase.database().child(user['localId']).child('Awards')
 dataset_db = firebase.database().child(user['localId']).child('Datasets')
 paper_db   = firebase.database().child(user['localId']).child('Papers')
+
+NN = NIH_NCBI()
 
 # Get all the datasets from Sparc Portal
 sparc_dataset_list = []
 sparc_dataset_list = get_list_of_datasets_with_metadata(sparc_dataset_list)
 
-for dataset in sparc_dataset_list:
-    # Find the awards associated with the datasets
-    
+disallowed_chars = {ord(c):None for c in "$#[]/. "}
 
+for dataset in sparc_dataset_list:
+    # Insert the dataset to the database
+    dataset_key = dataset['datasetDOI'].translate(disallowed_chars)
+    
+    dataset_record                  = {}
+    dataset_record['doi']           = dataset['datasetDOI']
+    dataset_record['name']          = dataset['name']
+    dataset_record['description']   = dataset['description']
+    dataset_record['project_num']   = dataset['properties']['award_id']
+    dataset_record['protocol_dois'] = dataset['protocolsDOI']
+    dataset_record['tags']          = dataset['tags']
+
+    db.child(user['localId']).child('Datasets').update({dataset_key: dataset_record}, user['idToken'])
+
+    # Find the funding awards (NIH) associated with the datasets. Add to award db
+    award_record              = NN.generateRecord(NN.getProjectFundingDetails([ dataset_record['project_num'] ]))
+    db.child(user['localId']).child('Awards').update({dataset_record['project_num']: award_record}, user['idToken'])
+    
+#    # Find papers associated with the dataset
+#    dataset_pub_records = NN.getPublicationsOfDataset(dataset_doi.split('./org')[1])
+#    for k in dataset_pub_records:
+#        dataset_pub_records[k]['dataset'] = dataset_doi
+#    
+#    # Find papers associated with the award
+#    funding_publications = {}
+#    for k in award_record:
+#        item = award_record[k]
+#        pub_record = NN.getPublications(item['appl_id'])
+#        funding_publications.update(pub_record)
+#    
+#    # Add the papers to the award db record
+#    award_record['papers']    = []
+#    for paper_doi in funding_publications:
+#        award_record.append(paper_doi)
+    
 
 
 #db.child(user['localId']).push('james', user['idToken'])
