953caf5dc260f6bc6a94bc65024915214d63e8a7
Sanjay Soundarajan <sanjays344@mail.fresnostate.edu>
2021-07-21 00:13:53 -0700

    Added force graph

--- sparc-portal/src/components/SparcLink/SparcLink.vue
+++ sparc-portal/src/components/SparcLink/SparcLink.vue
@@ -1,6 +1,8 @@
 <template>
   SPARClink
   <div id="svg_container" class="border-1 border-gray-100"></div>
+  container2
+  <div id="svg_container2" class="border-1 border-gray-100"></div>
 </template>
 
 <script>
@@ -184,10 +186,100 @@ function test() {
   console.log(split);
 }
 
+async function test2() {
+  const data = await d3.json(
+    "https://gist.githubusercontent.com/lodilas/613d35c4fbf91bb6892ecf0a4ff151a7/raw/4ca473162fccd8db1a8e0fbe22bf8542eaab3f94/JAS_2009-2018_neu.json"
+  );
+  console.log(data);
+
+  const height = 680;
+  const width = 1000;
+
+  const links = data.links.map((d) => Object.create(d));
+  const nodes = data.nodes.map((d) => Object.create(d));
+
+  // const svg = d3.create("svg")
+  //     .attr("viewBox", [-width / 2, -height / 2, width, height]);
+
+  var svg = d3.select("#svg_container2")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", [-width / 2, -height / 2, width, height]);
+
+  const simulation = d3
+    .forceSimulation(nodes)
+    .force(
+      "link",
+      d3.forceLink(links).id((d) => d.id)
+    )
+    .force("charge", d3.forceManyBody())
+    .force("x", d3.forceX())
+    .force("y", d3.forceY());
+
+  const link = svg
+    .append("g")
+    .attr("stroke", "#999")
+    .attr("stroke-opacity", 0.6)
+    .selectAll("line")
+    .data(links)
+    .join("line");
+
+  function dragstarted(d) {
+    console.log(d3);
+    console.log(currentEvent);
+    if (!d3.event.active) simulation.alphaTarget(1).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+  }
+
+  function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+  }
+
+  function dragended(d) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+  }
+
+  const node = svg
+    .append("g")
+    .attr("stroke", "#fff")
+    .attr("stroke-width", 1.5)
+    .selectAll("circle")
+    .data(nodes)
+    .join("circle")
+    .attr("r", 5)
+    .call(
+      d3
+        .drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended)
+    );
+
+  node.append("title").text((d) => d.id);
+
+  simulation.on("tick", () => {
+    link
+      .attr("x1", (d) => d.source.x)
+      .attr("y1", (d) => d.source.y)
+      .attr("x2", (d) => d.target.x)
+      .attr("y2", (d) => d.target.y);
+
+    node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+  });
+
+  console.log(svg, link, node);
+}
+
 export default {
   name: "SparcLink",
   mounted() {
     test();
+    test2();
   },
 };
 </script>
